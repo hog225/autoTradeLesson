138910ebdfcbbca62a188237dc2a5cf2e119ed50
yeonggi <epikhighjin@gmail.com>
2019-03-25 00:59:07 +0900

    KRX 에서 회사별 주식 코드 얻어오는 코드

--- getStockCode.py
+++ getStockCode.py
@@ -1,5 +1,22 @@
 import pandas as pd
 
-df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
 
-print(df.head())
+def getStockCode(market):
+    if market == 'kosdaq':
+        url_market = 'kosdaqMkt'
+    elif market == 'kospi':
+        url_market = 'stockMkt'
+    else:
+        print('invalid market ')
+        return
+
+    url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13&marketType=%s' % url_market
+    df = pd.read_html(url, header=0)[0]
+
+    return df
+
+
+if __name__ == '__main__':
+    result_df = getStockCode('kospi')
+    result_df.to_csv('kospi.csv')
+    print(result_df[['회사명', '종목코드']])
